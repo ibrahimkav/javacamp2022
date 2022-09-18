22a71758b6e50ad8375623c4ab954c919cd41dbf
pc <pc@DESKTOP-M9PT868.local>
2022-09-19 00:29:53 +0300

    datatypes

---
+++ datatypes/src/datatypes/Main.java
@@ -0,0 +1,16 @@
+package datatypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double sayi = 12.5;
+		sayi =-128;
+		System.out.println(sayi);
+		
+		char karakter = 'A';
+		
+		boolean dogruMu=false;
+
+	}
+
+}
