93f407f32bfc7a592087970c1ff6a9c75d33fea9
pc <pc@DESKTOP-M9PT868.local>
2022-09-19 00:02:21 +0300

    hello

---
+++ javaHello/src/javaHello/Main.java
@@ -0,0 +1,18 @@
+package javaHello;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Merhaba Java");
+		System.
+		out
+		.
+		println
+		(
+				"Merhaba Java 2"
+				)
+		;
+
+	}
+
+}
