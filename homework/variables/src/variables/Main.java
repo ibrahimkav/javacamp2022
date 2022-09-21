c0553aab4ce576fbba60a8a74c8d2a90e9774fea
İbrahim Kav <ibrahimkaav@gmail.com>
2022-09-21 22:00:55 +0300

    arrangement
    oneday

---
+++ homework/variables/src/variables/Main.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int ogrenciSayisi = 11;
+		String mesaj = "Ogrenci Sayisi :";
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println("Öğrenci sayım : 10");
+		System.out.println("Öğrenci sayım : 10");
+
+
+	}
+
+}
