d2236d2357cd3372e290ddd762ed548b808c5040
İbrahim Kav <ibrahimkaav@gmail.com>
2022-09-27 22:56:57 +0300

    day two homework

---
+++ homework/dayTwoHomework/methods/src/methods/Main.java
@@ -0,0 +1,31 @@
+package methods;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static void sayiBulma() {
+		int[] numbers = new int[] { 1, 2, 5, 7, 9, 0 };
+		int aranacak = 10;
+		boolean varMi = false;
+		for (int number : numbers) {
+			if (number == aranacak) {
+				varMi = true;
+				break;
+			}
+		}
+		if (varMi) {
+			mesajVer("Sayı Listede Bulunuyor." + aranacak);
+
+		} else {
+			mesajVer("Sayı listede yok." + aranacak);
+		}
+	}
+
+	public static void mesajVer(String mesaj) {
+		System.out.println(mesaj);
+	}
+
+}
