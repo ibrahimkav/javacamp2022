d27eb4c49d2ca623e842c3e8bbab2eb6fe8d85dd
İbrahim Kav <ibrahimkaav@gmail.com>
2022-10-12 10:38:45 +0300

    Polymorphism Demo

---
+++ homework/dayThreeHomework/homeworkTwo/polymorphismDemo/src/polymorphismDemo/Main.java
@@ -0,0 +1,14 @@
+package polymorphismDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+	//	BaseLogger[] loggers = new BaseLogger[] {new FileLogger(),new EmailLogger(),new DatabaseLogger()};
+	//	for (BaseLogger logger : loggers) {
+	//		logger.log("Log mesajı");
+	//	}
+		CustomerManager customerManager = new CustomerManager(new FileLogger());
+		customerManager.add();
+	}
+
+}
